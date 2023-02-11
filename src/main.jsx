8927eaa1aee84912c2f38705e5ac399f35fa998f
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-02-10 22:16:07 -0500

    Refactor main module and style.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
+import store from './redux/configureStore';
+import { apiSlice } from './redux/apis/apiSlice';
 import './main.css';
+
+store.dispatch(apiSlice.endpoints.getCountries.initiate());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
 );
