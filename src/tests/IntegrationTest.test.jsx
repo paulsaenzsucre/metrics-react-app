8317f257c0bb55a47884713c52a25f6c38cc2cee
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-02-11 00:55:37 -0500

    Add Integration Tests.

---
+++ src/tests/IntegrationTest.test.jsx
@@ -0,0 +1,82 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import {
+  test,
+  expect,
+  afterEach,
+  describe,
+  vi,
+} from 'vitest';
+import {
+  cleanup,
+  render,
+  screen,
+} from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, useParams } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import Header from '../components/Header';
+import App from '../components/App';
+import store from '../redux/configureStore';
+
+afterEach(cleanup);
+
+describe('Test App component', () => {
+  test('Should render App', () => {
+    const tree = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Test HomePage component', () => {
+  test('Should match HomePage snapshot', () => {
+    const tree = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      </BrowserRouter>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Should show big card', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      </BrowserRouter>,
+    );
+    expect(screen.getByText('WORLD (WORLD)')).toBeDefined();
+  });
+
+  test('Should show card list', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      </BrowserRouter>,
+    );
+    expect(screen.getByText('STATS BY COUNTRY')).toBeDefined();
+  });
+});
+
+describe('Test Header component', () => {
+  test('Should match Header snapshot', () => {
+    const tree = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      </BrowserRouter>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
